90886531ca44e5d72eff7909295509f9c5c52bd3
lumbricina <55181574+lumbricina@users.noreply.github.com>
2020-02-21 16:50:53 +0700

    hwaha

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -4,7 +4,7 @@ nama=$nama".log"
 nama=${nama// /_}
 pnama="pdkt_kusuma_"
 echo $nama
-for a in {1..29}
+for a in {1..28}
 do
  wget -a ${nama} -O ${pictname}$a "https://loremflickr.com/320/240/cat"
 done
